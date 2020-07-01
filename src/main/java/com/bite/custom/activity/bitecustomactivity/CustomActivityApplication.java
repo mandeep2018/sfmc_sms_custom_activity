ac53bef6f50cc227ee08d52b7762406f3ddd4bfc
mandeep2018 <mandeep.aj@gmail.com>
2020-07-01 10:15:26 +0530

    2nd commit

--- src/main/java/com/bite/custom/activity/bitecustomactivity/CustomActivityApplication.java
+++ src/main/java/com/bite/custom/activity/bitecustomactivity/CustomActivityApplication.java
@@ -3,11 +3,11 @@ package com.bite.custom.activity.bitecustomactivity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class CustomActivityApplication {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		SpringApplication.run(CustomActivityApplication.class, args);
-	}
+	}*/
 
 }
